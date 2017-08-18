538e50d2e6ff908a58fbeb878eb376cbb376d4fa
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    msm8916-common: init: Update for O

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -37,10 +37,11 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include <android-base/properties.h>
 #include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+
+using android::base::GetProperty;
 
 #define RAW_ID_PATH     "/sys/devices/soc0/raw_id"
 #define BUF_SIZE         64
@@ -69,14 +70,31 @@ static int read_file2(const char *fname, char *data, int max_size)
         return 0;
     }
 
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
+static void init_alarm_boot_properties()
+{
+    int boot_reason;
+    FILE *fp;
 
-    return 1;
+    fp = fopen("/proc/sys/kernel/boot_reason", "r");
+    fscanf(fp, "%d", &boot_reason);
+    fclose(fp);
+
+    /*
+     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+     * For existing PMIC chips, the following mapping applies
+     * for the value of boot_reason:
+     *
+     * 0 -> unknown
+     * 1 -> hard reset
+     * 2 -> sudden momentary power loss (SMPL)
+     * 3 -> real time clock (RTC)
+     * 4 -> DC charger inserted
+     * 5 -> USB charger inserted
+     * 6 -> PON1 pin toggled (for secondary PMICs)
+     * 7 -> CBLPWR_N pin toggled (for external power supply)
+     * 8 -> KPDPWR_N pin toggled (power key pressed)
+     */
+    property_set("ro.alarm_boot", boot_reason == 3 ? "true" : "false");
 }
 
 void property_override(char const prop[], char const value[])
